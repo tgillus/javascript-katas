cf604dfd27cc1455131d3f8277799b82b9cb6666
Tramaine Gillus <tramaine.gillus@gmail.com>
2018-12-24 16:11:28 -0500

    Add kata write-up.

--- test/coderbyte/letter-capitalize.test.js
+++ test/coderbyte/letter-capitalize.test.js
@@ -1,3 +1,9 @@
+// Letter Capitalize - https://www.coderbyte.com/results/tgillus:Letter%20Capitalize:JavaScript
+
+// Have the function LetterCapitalize(str) take the str parameter being passed
+// and capitalize the first letter of each word. Words will be separated by only
+// one space.
+
 const { letterCapitalize } = require('../../src/coderbyte/letter-capitalize');
 
 describe('letterCapitalize', () => {
