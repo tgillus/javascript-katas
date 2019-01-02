232c1d5ab5f842390f356613952a46268a3d710c
Tramaine Gillus <tramaine.gillus@gmail.com>
2019-01-01 23:16:45 -0500

    Refactor to reduce number of recursive call to avoid function stack overflow.

--- src/hackerrank/recursive-digit-sum.js
+++ src/hackerrank/recursive-digit-sum.js
@@ -1,20 +1,12 @@
 const superDigit = (number, repeats = 1) => {
-  number = repeatNumber(number, repeats);
+  if (number < 10) return number;
 
-  if (number.length === 1) return Number(number);
-
-  const digits = getDigits(number);
-  const sum = digits.reduce(sumDigits, 0);
+  const digits = ('' + number).split('');
+  const sum = digits.reduce(sumDigits, 0) * repeats;
 
   return superDigit(sum);
 };
 
-const repeatNumber = (number, repeats) => {
-  if (repeats <= 1) return number.toString();
-
-  return number.toString() + repeatNumber(number.toString(), repeats - 1);
-};
-const getDigits = number => number.toString().match(/[0-9]/g);
-const sumDigits = (sum, digit) => sum + Number(digit);
+const sumDigits = (sum, digit) => sum + +digit;
 
 module.exports = { superDigit };
--- test/hackerrank/recursive-digit-sum.test.js
+++ test/hackerrank/recursive-digit-sum.test.js
@@ -1,3 +1,23 @@
+// Recursive Digit Sum = https://www.hackerrank.com/challenges/recursive-digit-sum/problem
+
+// We define super digit of an integer x using the following rules:
+
+// Given an integer, we need to find the super digit of the integer. If x has
+// only 1 digit, then its super digit is x. Otherwise, the super digit of x is
+// equal to the super digit of the sum of the digits of x.
+
+// For example, the super digit of 9875 will be calculated as:
+//   super_digit(9875)     9 + 8 + 7 + 5 = 29 
+// 	 super_digit(29)               2 + 9 = 11
+// 	 super_digit(11)		           1 + 1 =  2
+// 	 super_digit(2)		                   =  2
+
+// You are given two numbers n and k. The number p is created by concatenating
+// the string n k times. Continuing the above example where n = 9875, assume
+// your value k = 4. Your initial p = 9875 9875 9875 9875 (spaces added for
+// clarity). All of the digits of p sum to 116. The digits of 116 sum to 8. 8 is
+// only one digit, so it's the super digit.
+
 const { superDigit } = require('../../src/hackerrank/recursive-digit-sum');
 
 describe('superDigit', () => {
