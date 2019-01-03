7d2468600abddc7d279046aec19ae5676e3e2ecf
Tramaine Gillus <tramaine.gillus@gmail.com>
2019-01-02 22:20:44 -0500

    Solve rna transcription kata.

---
+++ src/exercism/rna-transcription.js
@@ -0,0 +1,17 @@
+const toRna = dna => {
+  if (!dna) return dna;
+  if (isInvalid(dna)) throw new Error('Invalid input DNA.');
+
+  return dna.replace(/C|G|A|T/g, transribe);
+};
+
+const isInvalid = dna => !!dna.match(/[^CGAT]/);
+
+const transribe = nucleotide => {
+  if (nucleotide === 'C') return 'G';
+  if (nucleotide === 'G') return 'C';
+  if (nucleotide === 'A') return 'U';
+  return 'A';
+};
+
+module.exports = { toRna };
