09dce0a1923414225f05163bff36f756916d76f1
Tramaine Gillus <tramaine.gillus@gmail.com>
2019-01-06 13:42:14 -0500

    Fix typo.

--- src/coderbyte/letter-changes.js
+++ src/coderbyte/letter-changes.js
@@ -1,13 +1,13 @@
 const letterChanges = str => {
   const newStr = str.replace(/[a-z]/g, convertToNextLetterInAlphabet);
 
-  return newStr.replace(/a|e|i|o|u/g, converToUppercase);
+  return newStr.replace(/a|e|i|o|u/g, convertToUppercase);
 };
 
 const convertToNextLetterInAlphabet = char => {
   return char === 'z' ? 'a' : String.fromCharCode(char.charCodeAt(0) + 1);
 };
 
-const converToUppercase = char => char.toUpperCase();
+const convertToUppercase = char => char.toUpperCase();
 
 module.exports = { letterChanges };
