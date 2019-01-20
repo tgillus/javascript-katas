5126788edfb3af0323272663f9e46f1429c7b69e
Tramaine Gillus <tramaine.gillus@gmail.com>
2019-01-20 11:54:11 -0500

    Refactor to a more object-oriented solution.

--- src/hackerrank/time-conversion.js
+++ src/hackerrank/time-conversion.js
@@ -1,22 +1,65 @@
-const timeConversion = (standardTime) => {
-  const standardTimeRegex = /(\d{2}):(\d{2}):(\d{2})([AP]M)/;
-
-  return standardTime.replace(standardTimeRegex, militaryTime);
+const timeConversion = standardTime => {
+  return new StandardTime(standardTime).toMilitaryTime();
 };
 
-const militaryTime = (match, hour, minute, second, period) => {
-  return `${militaryHour(hour, period)}:${minute}:${second}`;
-};
+class StandardTime {
+  constructor(time) {
+    time.replace(
+      /(\d{2}):(\d{2}):(\d{2})([AP]M)/,
+      function(match, hour, minute, second, period) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+        this.period = period;
+      }.bind(this)
+    );
+  }
 
-const militaryHour = (hour, period) => {
-  const isMorning = period === 'AM';
-  const isMidnight = hour === '12' && isMorning;
-  const isNoon = hour === '12' && !isMorning;
+  toMilitaryTime() {
+    const standardHour = new StandardHour(
+      this.hour,
+      new StandardPeriod(this.period)
+    );
+    const militaryHour = standardHour.toMilitaryHour();
 
-  if (isMidnight) return '00';
-  if (isMorning || isNoon) return hour;
+    return `${militaryHour}:${this.minute}:${this.second}`;
+  }
+}
 
-  return `${+hour + 12}`;
-};
+class StandardPeriod {
+  constructor(period) {
+    this.period = period;
+  }
+
+  isAm() {
+    return this.period === 'AM';
+  }
+
+  isPm() {
+    return this.period === 'PM';
+  }
+}
+
+class StandardHour {
+  constructor(hour, standardPeriod) {
+    this.hour = hour;
+    this.standardPeriod = standardPeriod;
+  }
+
+  isMidnightHour() {
+    return this.hour === '12' && this.standardPeriod.isAm();
+  }
+
+  isNoonHour() {
+    return this.hour === '12' && this.standardPeriod.isPm();
+  }
+
+  toMilitaryHour() {
+    if (this.isMidnightHour()) return '00';
+    if (this.standardPeriod.isAm() || this.isNoonHour()) return this.hour;
+
+    return `${+this.hour + 12}`;
+  }
+}
 
 module.exports = { timeConversion };
