61f9d46611e2cf969dc47beb4201f104bab2f8e0
Tramaine Gillus <tramaine.gillus@gmail.com>
2019-01-30 19:40:52 -0500

    Add more text to explanation of kata.

--- test/hackerrank/plus-minus.test.js
+++ test/hackerrank/plus-minus.test.js
@@ -4,6 +4,14 @@
 // positive, negative, and are zeros. Print the decimal value of each fraction
 // on a new line.
 
+// For example, given the array arr = [1, 1, 0, -1, -1] there are  elements,
+// two positive, two negative and one zero. Their ratios would be
+// 2/5 = 0.400000, 2/5 = 0.400000, and 1/5 = 0.200000. It should be printed as:
+
+// 0.400000
+// 0.400000
+// 0.200000
+
 const { plusMinus } = require('../../src/hackerrank/plus-minus');
 
 describe('plusMinus', () => {
